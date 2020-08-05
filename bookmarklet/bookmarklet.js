a2265499c26afa898e9381abc6b5dc3bdc3bd7e6
Yasuyuki Suzuki <yasuzuki@gmail.com>
2020-08-05 11:21:06 +0900

    Create bookmarklet.js

---
+++ bookmarklet/bookmarklet.js
@@ -0,0 +1,4 @@
+
+
+
+(function(func) {var scr = document.createElement("script");scr.src = "//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js";scr.onload = function() {func(jQuery.noConflict(true));};document.body.appendChild(scr);})(function($) {   $('#sidebar').css({'width':'0%','display':'none'}); $('#content').css('width','100%'); });
