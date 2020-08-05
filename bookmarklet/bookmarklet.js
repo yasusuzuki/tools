c3366f13cedb7d2f7f2c3b6fbbb2653a52d71a32
Yasuyuki Suzuki <yasuzuki@gmail.com>
2020-08-05 11:45:24 +0900

    Update bookmarklet.js

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,4 +1,21 @@
 
 
+//Remove sidebar panel on Redmine
+$('#sidebar').css({'width':'0%','display':'none'});
+$('#content').css('width','100%');
 
-(function(func) {var scr = document.createElement("script");scr.src = "//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js";scr.onload = function() {func(jQuery.noConflict(true));};document.body.appendChild(scr);})(function($) {   $('#sidebar').css({'width':'0%','display':'none'}); $('#content').css('width','100%'); });
+
+//Remove control character from text on Gmail
+var copied = window.getSelection().toString();
+copied = copied.replace(/\r\n/g, '＞ ');
+console.log("aaaa"+copied);})();
+
+()=>{ document.addEventListener("copy", function (e) {  var copied = window.getSelection().toString();  console.log(copied);});}
+
+)()
+
+
+//Reset css
+javascript:$('body').css('-webkit-user-select','initial')
+
+
