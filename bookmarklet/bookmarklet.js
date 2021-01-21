ea94db12c0e720b9e741b236f36aed1e1e5ae330
Yasuyuki Suzuki <yasuzuki@gmail.com>
2021-01-21 23:39:12 +0900

    Update bookmarklet.js

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,3 +1,6 @@
+/***********************************************************************************************************************
+                     Gmail 
+************************************************************************************************************************/
 javascript:(()=>{
   var div = document.createElement("div"); 
   div.setAttribute("style","padding: 16px;border: 1px solid transparent;border-radius: 3px;border-color: #a0c7e4;background-color: #e1ecf4;margin: 0px 20px 0 20px;");
@@ -65,25 +68,36 @@ javascript:(()=>{
 })()
 
 
-//Remove sidebar panel on Redmine
-$('#sidebar').css({'width':'0%','display':'none'});$('#content').css('width','100%');
+//Remove sidebar panel on Redmine, then remove font-family as Hiragino font doesn't exist
+javascript:(function(func) {var scr = document.createElement("script");scr.src = "//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";scr.onload = function() {func(jQuery.noConflict(true));};document.body.appendChild(scr);})(function($) {
+    $('#sidebar').css({'width':'0%','display':'none'}); $('#content').css('width','100%');
+    $('body').css('font-family','');
+});
+
 
 //Remove control character from text on Gmail
 //
 //* Stack Overflow javascript <-> clipboard interaction https://stackoverflow.com/questions/400212
 javascript:(()=>{document.addEventListener("copy", function (e) { var copied = window.getSelection().toString(); copied = copied.replace(/[\x0a\x0d]/g, '');e.clipboardData.setData('text/plain', copied);e.preventDefault();});})()
 //jQuery version
-$(document).on("copy",function (e) { var copied = window.getSelection().toString(); copied = copied.replace(/[\x0a\x0d]/g, '');e.clipboardData.setData('text/plain', copied);e.preventDefault();})
-
+javascript:(function(func) {var scr = document.createElement("script");scr.src = "//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";scr.onload = function() {func(jQuery.noConflict(true));};document.body.appendChild(scr);})(function($) {
+    $(document).on("copy",function (e) { var copied = window.getSelection().toString();
+                                        copied = copied.replace(/[\x0a\x0d]/g, '');
+                                        e.clipboardData.setData('text/plain', copied);
+                                        e.preventDefault();})
+});
 
 //Remove Copy Guard
 // Some Web site disables Ctrl+c or from right click operation by CSS. This bookmarklet reset the css and enable Copy.
-$('body').css('-webkit-user-select','initial')
+javascript:(function(func) {var scr = document.createElement("script");scr.src = "//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";scr.onload = function() {func(jQuery.noConflict(true));};document.body.appendChild(scr);})(function($) {
+    $('body').css('-webkit-user-select','initial')
+});
 
 //Highlight token  
 //
 //* this is useful for Wiki page which doesn't support text highlight very well
 //* this highlights particular characters such as "☆" on TODO or conclusion or anything you want to emphasize
 //* Highlighting any text starting from "☆" until 全角スペース(%E3%80%80)
-$("body").html( $("body").html().replace(/(☆.*?)%E3%80%80/g, '<span style="background-color:#FFCBCE">$1&nbsp;</span>'));$("body").html( $("body").html().replace(/(★.*?)%E3%80%80/g, '<span style="background-color:#FCFFCB">$1&nbsp;</span>'));
-
+javascript:(function(func) {var scr = document.createElement("script");scr.src = "//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";scr.onload = function() {func(jQuery.noConflict(true));};document.body.appendChild(scr);})(function($) {
+    $("body").html( $("body").html().replace(/(☆.*?)%E3%80%80/g, '<span style="background-color:#FFCBCE">$1&nbsp;</span>'));$("body").html( $("body").html().replace(/(★.*?)%E3%80%80/g, '<span style="background-color:#FCFFCB">$1&nbsp;</span>'));
+});
